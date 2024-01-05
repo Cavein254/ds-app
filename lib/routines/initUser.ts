6bfb0e9f293c74627082de73e917ecf8358f140d
johnmuthua <caveomuthama@gmail.com>
2024-01-05 17:10:44 +0300

    add: updated profile schema

---
+++ lib/routines/initUser.ts
@@ -0,0 +1,30 @@
+import prisma from '@/lib/prismadb';
+
+export const initialUser = async () => {
+  //TODO :: Get user email from NextAuth
+  const nextEmail = {
+    id: 'x4563dtyr67835',
+    first_name: 'cavein',
+    last_name: 'Get',
+    imageUrl: 'large url',
+    email: 'cavein@get.com',
+  };
+  const profile = await prisma.profile.findFirst({
+    where: {
+      userId: nextEmail.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await prisma.profile.create({
+    data: {
+      userId: nextEmail.id,
+      name: `${nextEmail.first_name} ${nextEmail.last_name}`,
+      imageUrl: nextEmail.imageUrl,
+      email: nextEmail.email,
+    },
+  });
+};
