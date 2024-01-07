4b073893a0e077a1389c0eb6e7c3d081ab272df0
johnmuthua <caveomuthama@gmail.com>
2024-01-07 11:24:11 +0300

    add redux

--- components/nav/NavItem.tsx
+++ components/nav/NavItem.tsx
@@ -1,9 +1,19 @@
+"use client"
+
 import Dog from '@/assets/dog2_edited.png';
 import Image from 'next/image';
+
+import { Actio } from ;
 const NavItem = () => {
   return (
-    <div>
-      <Image src={Dog} alt="tribe image" height={50} width={50} />
+    <div className="pt-2">
+      <Image
+        src={Dog}
+        alt="tribe image"
+        height={50}
+        width={50}
+        className="contain rounded-full"
+      />
     </div>
   );
 };
